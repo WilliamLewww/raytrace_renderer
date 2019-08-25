a504363d12b8c2e385bd707dbed76a5471e58567
William Lew <WilliamMilesLew@gmail.com>
2019-08-25 12:59:00 -0700

    generalized matrix to not just handle 4x4

--- src_CPU/main.cpp
+++ src_CPU/main.cpp
@@ -9,41 +9,17 @@ int main(int argc, const char** argv) {
 	Canvas* canvas = new Canvas(SCREENWIDTH, SCREENHEIGHT);
 	canvas->saveToFile(argv[1]);
 
-	Matrix4 matrixA = createMatrix4();
-	Matrix4 matrixB = createMatrix4();
-	matrixA[0][0] = 1;
-	matrixA[0][1] = 2;
-	matrixA[0][2] = 3;
-	matrixA[0][3] = 4;
-	matrixA[1][0] = 5;
-	matrixA[1][1] = 6;
-	matrixA[1][2] = 7;
-	matrixA[1][3] = 8;
-	matrixA[2][0] = 9;
-	matrixA[2][1] = 8;
-	matrixA[2][2] = 7;
-	matrixA[2][3] = 6;
-	matrixA[3][0] = 5;
-	matrixA[3][1] = 4;
-	matrixA[3][2] = 3;
-	matrixA[3][3] = 2;
+	Matrix matrixA = createMatrix(3, 3);
+	matrixA[0][0] = 3;
+	matrixA[0][1] = 5;
+	matrixA[0][2] = 0;
+	matrixA[1][0] = 2;
+	matrixA[1][1] = -1;
+	matrixA[1][2] = -7;
+	matrixA[2][0] = 6;
+	matrixA[2][1] = -1;
+	matrixA[2][2] = 5;
 
-	matrixB[0][0] = -2;
-	matrixB[0][1] = 1;
-	matrixB[0][2] = 2;
-	matrixB[0][3] = 3;
-	matrixB[1][0] = 3;
-	matrixB[1][1] = 2;
-	matrixB[1][2] = 1;
-	matrixB[1][3] = -1;
-	matrixB[2][0] = 4;
-	matrixB[2][1] = 3;
-	matrixB[2][2] = 6;
-	matrixB[2][3] = 5;
-	matrixB[3][0] = 1;
-	matrixB[3][1] = 2;
-	matrixB[3][2] = 7;
-	matrixB[3][3] = 8;
 
-	std::cout << (matrixA * matrixB) << std::endl;
+	std::cout << cofactor(matrixA, 0, 0) << std::endl;
 }
--- src_CPU/matrix.h
+++ src_CPU/matrix.h
@@ -2,23 +2,30 @@
 
 //FIX MEMORY LEAK
 
-struct Matrix4 {
+struct Matrix {
 	float** data;
+
+	int rowCount;
+	int columnCount;
 
 	float* operator[](int x) {
 		return data[x];
 	};
 };
 
-const float getElementFromMatrix(Matrix4 matrix, int x, int y) {
+const float getElementFromMatrix(Matrix matrix, int x, int y) {
 	return matrix[x][y];
 }
 
-bool operator==(Matrix4 matrixA, Matrix4 matrixB) {
+bool operator==(Matrix matrixA, Matrix matrixB) {
 	bool greater = false;
 
-	for (int x = 0; !greater && x < 4; x++) {
-		for (int y = 0; !greater && y < 4; y++) {
+	if (matrixA.rowCount != matrixB.rowCount || matrixA.columnCount != matrixB.columnCount) {
+		return false;
+	}
+
+	for (int x = 0; !greater && x < matrixA.rowCount; x++) {
+		for (int y = 0; !greater && y < matrixA.columnCount; y++) {
 			if (abs(matrixA[x][y] - matrixB[x][y]) >= EPSILON_TUPLE) {
 				greater = true;
 			}
@@ -28,44 +35,115 @@ bool operator==(Matrix4 matrixA, Matrix4 matrixB) {
 	return !greater;
 }
 
-std::ostream& operator<<(std::ostream& os, const Matrix4& matrix) {
-	for (int x = 0; x < 4; x++) {
+std::ostream& operator<<(std::ostream& os, const Matrix& matrix) {
+	for (int x = 0; x < matrix.rowCount; x++) {
 		os << "[";
-		for (int y = 0; y < 4; y++) {
+		for (int y = 0; y < matrix.columnCount; y++) {
 			os << getElementFromMatrix(matrix, x, y);
-			if (y < 3) { os << ", "; }
+			if (y < matrix.columnCount - 1) { os << ", "; }
 		}
 		os << "]";
-		if (x < 3) { os << "\n"; }
+		if (x < matrix.rowCount - 1) { os << "\n"; }
 		
 	}
 
 	return os;
 }
 
-Matrix4 createMatrix4() {
-	Matrix4 temp;
-	temp.data = new float*[4];
-	for (int x = 0; x < 4; x++) {
-		temp.data[x] = new float[4];
-		for (int y = 0; y < 4; y++) {
+Matrix createMatrix(int rowCount, int columnCount) {
+	Matrix temp;
+	temp.data = new float*[rowCount];
+	for (int x = 0; x < rowCount; x++) {
+		temp.data[x] = new float[columnCount];
+		for (int y = 0; y < columnCount; y++) {
 			temp.data[x][y] = 0.0;
+		}
+	}
+
+	temp.rowCount = rowCount;
+	temp.columnCount = columnCount;
+
+	return temp;
+}
+
+Matrix createIdentityMatrix(int rowCount) {
+	Matrix temp = createMatrix(rowCount, rowCount);
+	for (int x = 0; x < rowCount; x++) {
+		temp[x][x] = 1.0;
+	}
+
+	return temp;
+}
+
+Matrix operator*(Matrix matrixA, Matrix matrixB) {
+	Matrix temp = createMatrix(matrixA.rowCount, matrixB.columnCount);
+	for (int x = 0; x < matrixA.rowCount; x++) {
+		for (int y = 0; y < matrixB.columnCount; y++) {
+			for (int z = 0; z < matrixA.columnCount; z++) {
+				temp[x][y] += (matrixA[x][z] * matrixB[z][y]);
+			}
 		}
 	}
 
 	return temp;
 }
 
-Matrix4 operator*(Matrix4 matrixA, Matrix4 matrixB) {
-	Matrix4 temp = createMatrix4();
-	for (int x = 0; x < 4; x++) {
-		for (int y = 0; y < 4; y++) {
-			temp[x][y] = ((matrixA[x][0] * matrixB[0][y]) +
-						  (matrixA[x][1] * matrixB[1][y]) +
-						  (matrixA[x][2] * matrixB[2][y]) +
-						  (matrixA[x][3] * matrixB[3][y]));
+Tuple operator*(Matrix matrix, Tuple tuple) {
+	return {
+		((matrix[0][0] * tuple.x) + (matrix[0][1] * tuple.y) + (matrix[0][2] * tuple.z) + (matrix[0][3] * tuple.w)),
+ 		((matrix[1][0] * tuple.x) + (matrix[1][1] * tuple.y) + (matrix[1][2] * tuple.z) + (matrix[1][3] * tuple.w)),
+ 		((matrix[2][0] * tuple.x) + (matrix[2][1] * tuple.y) + (matrix[2][2] * tuple.z) + (matrix[2][3] * tuple.w)),
+ 		((matrix[3][0] * tuple.x) + (matrix[3][1] * tuple.y) + (matrix[3][2] * tuple.z) + (matrix[3][3] * tuple.w))
+	};
+}
+
+Matrix transpose(Matrix matrix) {
+	Matrix temp = createMatrix(matrix.columnCount, matrix.rowCount);
+	for (int x = 0; x < matrix.rowCount; x++) {
+		for (int y = 0; y < matrix.columnCount; y++) {
+			temp[y][x] = matrix[x][y];
 		}
 	}
 
 	return temp;
+}
+
+float determinate(Matrix matrix) {
+	return ((matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1]));
+}
+
+Matrix submatrix(Matrix matrix, int row, int column) {
+	Matrix temp = createMatrix(matrix.rowCount - 1, matrix.columnCount - 1);
+
+	int currentX = 0;
+	int currentY = 0;
+	for (int x = 0; x < matrix.rowCount; x++) {
+		for (int y = 0; y < matrix.columnCount; y++) {
+			if (x != row && y != column) {
+				temp[currentX][currentY] = matrix[x][y];
+				if (currentY + 1 == temp.columnCount) {
+					currentX += 1;
+					currentY = 0;
+				}
+				else {
+					currentY += 1;
+				}
+			}
+		}
+	}
+
+	return temp;
+}
+
+float minor(Matrix matrix, int row, int column) {
+	Matrix temp = submatrix(matrix, row, column);
+	return determinate(temp);
+}
+
+float cofactor(Matrix matrix, int row, int column) {
+	if ((row + column) % 2 == 0) {
+		return minor(matrix, row, column);
+	}
+
+	return -minor(matrix, row, column);
 }
