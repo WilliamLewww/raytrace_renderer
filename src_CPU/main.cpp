24edc2c8a45c0419cab1fe406d1520c46bc5d861
William Lew <WilliamMilesLew@gmail.com>
2019-08-25 14:03:06 -0700

    created translation, scale, and rotation transformation matricies

--- src_CPU/main.cpp
+++ src_CPU/main.cpp
@@ -1,6 +1,7 @@
 #include "canvas.h"
 #include "tuple.h"
 #include "matrix.h"
+#include "transform.h"
 
 const int SCREENWIDTH = 100;
 const int SCREENHEIGHT = 60;
@@ -9,23 +10,8 @@ int main(int argc, const char** argv) {
 	Canvas* canvas = new Canvas(SCREENWIDTH, SCREENHEIGHT);
 	canvas->saveToFile(argv[1]);
 
-	Matrix matrixA = createMatrix(4, 4);
-	matrixA[0][0] = -5;
-	matrixA[0][1] = 2;
-	matrixA[0][2] = 6;
-	matrixA[0][3] = -8;
-	matrixA[1][0] = 1;
-	matrixA[1][1] = -5;
-	matrixA[1][2] = 1;
-	matrixA[1][3] = 8;
-	matrixA[2][0] = 7;
-	matrixA[2][1] = 7;
-	matrixA[2][2] = -6;
-	matrixA[2][3] = -7;
-	matrixA[3][0] = 1;
-	matrixA[3][1] = -3;
-	matrixA[3][2] = 7;
-	matrixA[3][3] = 4;
+	Tuple point = createPoint(0, 1, 0);
+	Matrix transformation = createRotationMatrixZ(M_PI / 4);
 
-	std::cout << inverse(matrixA) << std::endl;
+	std::cout << transformation * point << std::endl;
 }
---
+++ src_CPU/transform.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <cmath>
+#include "matrix.h"
+
+Matrix createTranslateMatrix(float x, float y, float z) {
+	Matrix temp = createIdentityMatrix(4);
+	temp[0][3] = x;
+	temp[1][3] = y;
+	temp[2][3] = z;
+
+	return temp;
+}
+
+Matrix createScaleMatrix(int x, int y, int z) {
+	Matrix temp = createIdentityMatrix(4);
+	temp[0][0] = x;
+	temp[1][1] = y;
+	temp[2][2] = z;
+
+	return temp;
+}
+
+Matrix createRotationMatrixX(float radians) {
+	Matrix temp = createIdentityMatrix(4);
+	temp[1][1] = cos(radians);
+	temp[1][2] = -sin(radians);
+	temp[2][1] = sin(radians);
+	temp[2][2] = cos(radians);
+
+	return temp;
+}
+
+Matrix createRotationMatrixY(float radians) {
+	Matrix temp = createIdentityMatrix(4);
+	temp[0][0] = cos(radians);
+	temp[0][2] = sin(radians);
+	temp[2][0] = -sin(radians);
+	temp[2][2] = cos(radians);
+
+	return temp;
+}
+
+Matrix createRotationMatrixZ(float radians) {
+	Matrix temp = createIdentityMatrix(4);
+	temp[0][0] = cos(radians);
+	temp[0][1] = -sin(radians);
+	temp[1][0] = sin(radians);
+	temp[1][1] = cos(radians);
+
+	return temp;
+}
