5120b8c13fb45eb55b96a501ff54fdbfd29fcd92
WilliamLewww <WilliamMilesLew@gmail.com>
2019-08-30 17:38:24 -0700

    separated sample cuda code to its own file

---
+++ src_GPU/sample.cu
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <math.h>
+
+__global__
+void add(int n, float *x, float *y) {
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	int stride = blockDim.x * gridDim.x;
+	for (int i = index; i < n; i += stride) {
+		y[i] = x[i] + y[i];
+	}
+}
+
+int main() {
+	int count = 100;
+	float *first, *second;
+
+	cudaMallocManaged(&first, count*sizeof(float));
+	cudaMallocManaged(&second, count*sizeof(float));
+
+	for (int x = 0; x < count; x++) {
+		first[x] = x;
+		second[x] = x;
+	}
+
+	int blockSize = 256;
+	int numBlocks = (count + blockSize - 1) / blockSize;
+	add<<<numBlocks, blockSize>>>(count, first, second);
+
+	cudaDeviceSynchronize();
+
+	for (int x = 0; x < count; x++) {
+		std::cout << second[y] << std::endl;
+	}
+
+	cudaFree(first);
+	cudaFree(second);
+
+	return 0;
+}
