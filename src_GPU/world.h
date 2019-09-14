3c3af7e2499bf21ff09f6f39edd32cf2c6a1207f
William Lew <WilliamMilesLew@gmail.com>
2019-09-13 20:32:42 -0700

    created device function "intersectWorldCount"

--- src_GPU/world.h
+++ src_GPU/world.h
@@ -25,26 +25,44 @@ World createDefaultWorld() {
 	return world;
 }
 
+__device__
+int intersectWorldCount(Shape* shapes, int shapeCount, Ray ray) {
+	int intersectionCount = 0;
+	
+	return intersectionCount;
+}
+
 __global__
-void colorAtKernel(Tuple* colorBuffer, World world, Ray* rays, int rayCount) {
+void colorAtKernel(Tuple* colorBuffer, Ray* rays, int rayCount, Shape* shapes, int shapeCount) {
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
 	int stride = blockDim.x * gridDim.x;
 	for (int x = index; x < rayCount; x += stride) {
-		colorBuffer[x] = { 0, 0, 0, 1 };
+		int intersectionCount = intersectWorldCount(shapes, shapeCount, rays[x]);
+
+		if (intersectionCount > 0) {
+			colorBuffer[x] = { 1, 0, 0, 1 };
+		}
+		else {
+			colorBuffer[x] = { 0, 0, 0, 1 };
+		}
 	}
 }
 
 void colorAt(Tuple* colorOut, World world, Ray* rays, int rayCount) {
 	Tuple* colorBuffer;
+	Shape* shapeBuffer;
 
 	cudaMallocManaged(&colorBuffer, rayCount*sizeof(Tuple));
+	cudaMallocManaged(&shapeBuffer, world.shapeCount*sizeof(Shape));
+	memcpy(shapeBuffer, world.shapeArray, world.shapeCount*sizeof(Shape));
 
 	int blockSize = 256;
 	int numBlocks = (rayCount + blockSize - 1) / blockSize;
-	colorAtKernel<<<numBlocks, blockSize>>>(colorBuffer, world, rays, rayCount);
+	colorAtKernel<<<numBlocks, blockSize>>>(colorBuffer, rays, rayCount, shapeBuffer, world.shapeCount);
 
 	cudaDeviceSynchronize();
 	cudaMemcpy(colorOut, colorBuffer, rayCount*sizeof(Tuple), cudaMemcpyDeviceToHost);
 
 	cudaFree(colorBuffer);
+	cudaFree(shapeBuffer);
 }
